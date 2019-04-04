530305506f7c5610107100ab3e41c56a5aa62086
Erick Guan <fantasticfears@gmail.com>
2019-04-05 00:30:24 +0200

    add unit test case

--- test_syllable_segmentation.cpp
+++ test_syllable_segmentation.cpp
@@ -5,10 +5,32 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "syllable_segmentation.hpp"
 
-TEST_CASE( "Factorials are computed", "[factorial]") {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("AppendPhone can be appended with more phones", "[unit]") {
+  SyllableSegmentor s;
+  REQUIRE_NOTHROW(s.AppendPhone('c'));
+}
+
+TEST_CASE("GetSyllableList return a list of syllable list", "[unit]") {
+  SyllableSegmentor s1;
+  for (auto c : "fangan") {
+    s1.AppendPhone(c);
+  }
+  SECTION("Contains solutions") {
+    CHECK_THAT(s1, VectorContains("fang`an"));
+    CHECK_THAT(s1, VectorContains("fan`gan"));
+  }
+
+  SyllableSegmentor s2;
+  for (auto c : "xiangang") {
+    s2.AppendPhone(c);
+  }
+  SECTION("Contains solutions") {
+    CHECK_THAT(s2, VectorContains("xi`an`gang"));
+    CHECK_THAT(s2, VectorContains("xian`gang"));
+    CHECK_THAT(s2, VectorContains("xiang`ang"));
+    CHECK_THAT(s2, VectorContains("xi`ang`ang"));
+  }
+
 }
