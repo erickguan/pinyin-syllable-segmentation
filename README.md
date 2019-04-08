ef5399be6f9fb37888d7f57f789f6350428fed0a
Erick Guan <fantasticfears@gmail.com>
2019-04-08 09:25:37 +0200

    improve fuzzer performance

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pinyin Syllable Segmentation
+
+A simple header-only library list all possible syllable lists from a sentence.
+
+It's intended for Chinese input method that based on pinyin.
+
+`syllable_list.csv` is a dict file of available syllables with the frequency information.
--- test_fuzz.cpp
+++ test_fuzz.cpp
@@ -1,16 +1,13 @@
 #include "syllable_segmentation.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
-  epinyin::SyllableSegmentor s(epinyin::SyllableIndex::CreateShared("syllable_list.csv"));
+  static auto index = epinyin::SyllableIndex::CreateShared("syllable_list.csv");
+  epinyin::SyllableSegmentor s(index);
 
   for (auto i = 0; i < Size; ++i) {
     s.AppendPhone(Data[i]);
   }
   s.GetSyllableList();
-  if (s.size() > 1) {
-    s.PopLastPhone();
-    s.GetSyllableList();
-  }
 
   return 0;  // Non-zero return values are reserved for future use.
 }
